fb03fa64365a4f527a04c4770c6326cbcf134197
Evan Kiel <kielev@dc22.cis.gvsu.edu>
2017-05-09 19:54:55 -0400

    :Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     Committer: Evan Kiel <kielev@dc22.cis.gvsu.edu>
    
     On branch master
     Changes to be committed:
       (use "git reset HEAD <file>..." to unstage)
    
    	modified:   src/my_functions.cpp

---
+++ src/my_functions.cpp
@@ -0,0 +1,27 @@
+#include <vector>
+#include <string>
+
+std::string createString(){
+        std::string j("Hello world!");
+        return j;
+}
+
+
+std::vector<std::string> createVector(){
+        std::vector<std::string> j;
+        return j;
+}
+
+
+int putStringsInVector(int num){
+        std::vector<std::string> stringVector = createVector();
+        for(int i = 0; i < num; i++)
+                stringVector.emplace_back(createString());
+        return num;
+}
+
+void modifyVector(std::vector<int> & vector){
+        for(unsigned int i =  4; i < vector.size(); i+=5)
+                vector.at(i) = 42;
+}
+
