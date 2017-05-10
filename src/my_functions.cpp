89d45e4329169ce25dcf93cca5c25d67eadd2750
Evan Kiel <kielev@dc22.cis.gvsu.edu>
2017-05-09 20:26:01 -0400

    modified:   ../include/my_functions.h

--- src/my_functions.cpp
+++ src/my_functions.cpp
@@ -1,18 +1,33 @@
+/*****************************************************************
+ * Homework 1 solution working with vectors and strings including 
+ * references and guards, Basic resuable functions for string and
+ * vectors
+ *
+ * @author Evan Kiel
+ * @version 5/9/2017
+ * *****************************************************************/
+
+/** Required includes for vectors and strings */
 #include <vector>
 #include <string>
 
+/** Function to create and return string containing Hello world! */
 std::string createString(){
         std::string j("Hello world!");
         return j;
 }
 
-
+/** Fuction to create and return vector containing a string */
 std::vector<std::string> createVector(){
         std::vector<std::string> j;
         return j;
 }
 
 
+/*****************************************************************
+Creates a vector that that contains num number of strings
+Returns the number of strings that was stored            
+****************************************************************/
 int putStringsInVector(int num){
         std::vector<std::string> stringVector = createVector();
         for(int i = 0; i < num; i++)
@@ -20,6 +35,10 @@ int putStringsInVector(int num){
         return num;
 }
 
+/*****************************************************************
+References a vector and changes every fifth element to 42
+Starts at 0 and adds five(0,5,10,15...)
+*****************************************************************/
 void modifyVector(std::vector<int> & vector){
         for(unsigned int i =  0; i < vector.size(); i+=5)
                 vector.at(i) = 42;
